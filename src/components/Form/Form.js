07ebbbf78b528b77add6facaaff1d6f25d12c267
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-08-05 19:44:40 +0600

    first commit

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css'
+
+const Form = () => {
+    return (
+        <div className='header'>
+            <input type="email" name="" id="" /><br />
+            <button>Submit</button>
+            
+        </div>
+    );
+};
+
+export default Form;
